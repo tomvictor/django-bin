70c214f30440e69e75cc5c9dcda1cc789cc0cbed
Tom Victor <vjtomvictor@icloud.com>
2017-02-21 21:39:23 +0530

    Error fixed

--- djangobin/MainApp/urls.py
+++ djangobin/MainApp/urls.py
@@ -4,7 +4,7 @@ from MainApp import views
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^login/$',views.login,name='login'),
-    url(r'^logout/$',views.logout,name='logout'),
+    url(r'^logout/$',views.logout_view,name='logout'),
     url(r'^single/$',views.single_post,name='single-post'),
     url(r'^user/$',views.single_user,name='single-user'),
     url(r'^loginform/$',views.login_form,name='login-form-handler'),
