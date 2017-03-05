33da3b100522bdee3894ae2656b5764777d22514
Tom Victor <vjtomvictor@icloud.com>
2017-03-05 23:10:53 +0530

    Working , just ok

--- djangobin/MainApp/urls.py
+++ djangobin/MainApp/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.home.as_view(template_name='home.html'),name='home'),
+    url(r'^all/$',views.home.as_view(template_name='all-posts.html'),name='view-all'),
     url(r'^login/$',views.login,name='login'),
     url(r'^logout/$',views.logout_view,name='logout'),
     url(r'^signup/$',views.sign_up_view,name='sign-up'),
     url(r'^single/$',views.single_post,name='single-post'),
     url(r'^user/$',views.single_user,name='single-user'),
-    url(r'^all/$',views.all_posts.as_view(),name='view-all'),
     #forms
     url(r'^loginform/$',views.login_form,name='login-form-handler'),
     url(r'^signupform/$',views.signup_form,name='signup-form-handler'),
