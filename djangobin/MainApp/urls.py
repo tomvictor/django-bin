0ea63b8ebcf9acd858a97cac0b1f450124a2134a
Tom Victor <vjtomvictor@icloud.com>
2017-03-19 02:29:40 +0530

    Backup code added

--- djangobin/MainApp/urls.py
+++ djangobin/MainApp/urls.py
@@ -10,11 +10,7 @@ urlpatterns = [
     url(r'^postformhandle/$',views.post_form_upload,name='post-form'),
     url(r'^$',views.base_view.as_view(template_name='home.html'),name='home'),
     url(r'^all/$',views.base_view.as_view(template_name='all-posts.html'),name='view-all'),
-<<<<<<< HEAD
     # url(r'^private/$',views.privatePosts.as_view(template_name='private-posts.html'),name='view-private'),
-=======
-    url(r'^(?P<slug>[-\w]+)/$', views.post_detail_view.as_view(template_name='detail-view.html'), name='detail'),
->>>>>>> origin/master
     url(r'^login/$',views.login,name='login'),
     url(r'^logout/$',views.logout_view,name='logout'),
     url(r'^signup/$',views.sign_up_view,name='sign-up'),
